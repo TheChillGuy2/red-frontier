55745190455cf1306b15f54c71330d21766da35f
TheChillGuy2 <maximilianseebacher13@gmail.com>
2025-04-16 15:56:51 +0100

    Create meta.js

---
+++ js/meta.js
@@ -0,0 +1,23 @@
+function setMetaTag(name, content) {
+  let tag = document.querySelector(`meta[name="${name}"]`);
+  if (!tag) {
+    tag = document.createElement('meta');
+    tag.setAttribute('name', name);
+    document.head.appendChild(tag);
+  }
+  tag.setAttribute('content', content);
+}
+
+function setPageTitle(title) {
+  document.title = title;
+}
+
+function setOpenGraphMeta(property, content) {
+  let tag = document.querySelector(`meta[property="${property}"]`);
+  if (!tag) {
+    tag = document.createElement('meta');
+    tag.setAttribute('property', property);
+    document.head.appendChild(tag);
+  }
+  tag.setAttribute('content', content);
+}
